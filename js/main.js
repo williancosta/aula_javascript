7e32c6b0324d4ab3c1788b6a2d69bea96d61a260
williancosta <willianjac@hotmail.com>
2021-05-14 11:19:43 -0300

    add index.html e main.js

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var nome ="usuário";
+var idade = 39;
+//alert("Mensagem usando alert na página");
+alert(nome+" tem "+idade+" anos");
+/* Roteiro Javascript
+alert
+variaveis
+console.log
+array e dicionario
+condicional (if else)
+laços de repetição while e for
+push, pop e length
+reverse array
+toString para array
+join para array
+variavel global/local
+replace
+lowerCase, upperCase
+redirecionar para outra página
+date
+function
+*/
+
+/* dom
+getElement
+innerHTML
+onclick
+onmouseover
+onchange
+onload
+onmouseover
+onmouseout
+*/
